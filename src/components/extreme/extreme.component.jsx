cde4ba144cb156d72cac7c37b7c3ec775128485c
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-09-19 04:17:52 +0530

    Add extreme running component

---
+++ src/components/extreme/extreme.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './extreme.styles.scss';
+
+import leftHand from '../../assets/samples/leftHand.svg';
+import serverBodyExtreme from '../../assets/samples/server-4.svg';
+import rightHand from '../../assets/samples/rightHand.svg';
+
+const Extreme = () => (
+  <div className='brisk-walking'>
+    <img src={serverBodyExtreme} className="serverBodyExtreme" alt="serverBody" />
+    <img src={leftHand} className="leftHandExtreme" alt="leftHand" />
+    <img src={rightHand} className="rightHandExtreme" alt="rightHand" />
+  </div>
+);
+
+export default Extreme;
