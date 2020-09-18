88c54cd12e9229e4f65d8c83f67b1800e0d18eff
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-09-19 03:02:56 +0530

    Add controls

---
+++ src/containers/controls/controls.container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './controls.styles.scss';
+
+const ControlsContainer = () => (
+  <div className='controls'>
+    <h1>This is controls</h1>
+  </div>
+);
+
+export default ControlsContainer;
