526d218b1376ae901de2fcde84a68dc380122c23
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-09-20 03:07:02 +0530

    Completed with extreme running component

--- src/components/extreme/extreme.component.jsx
+++ src/components/extreme/extreme.component.jsx
@@ -5,12 +5,14 @@ import './extreme.styles.scss';
 import leftHand from '../../assets/samples/leftHand.svg';
 import serverBodyExtreme from '../../assets/samples/server-4.svg';
 import rightHand from '../../assets/samples/rightHand.svg';
+import BackPackComponent from '../backpack/backpack.component';
 
 const Extreme = () => (
   <div className='brisk-walking'>
     <img src={serverBodyExtreme} className="serverBodyExtreme" alt="serverBody" />
     <img src={leftHand} className="leftHandExtreme" alt="leftHand" />
     <img src={rightHand} className="rightHandExtreme" alt="rightHand" />
+    <BackPackComponent/>
   </div>
 );
 
