c8eeb7b4475bff026e737a33a152d9702929006e
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-09-20 03:08:19 +0530

    Completed with canvas container

--- src/containers/canvas/canvas.container.jsx
+++ src/containers/canvas/canvas.container.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 import './canvas.styles.scss';
@@ -6,32 +7,26 @@ import Walking from '../../components/walking/walking.component';
 import BriskWalking from '../../components/brisk-walking/brisk-walking.component';
 import Running from '../../components/running/running.component';
 import Extreme from '../../components/extreme/extreme.component';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCurrentSillyRangeValues } from '../../redux/silly-range-values/silly-range-values.selector';
 
 class CanvasContainer extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = ({
-      serverState: "walking",
-      stateCount: 0,
-      stateVariables: ["walking", "briskWalking", "running", "extreme"],
-    });
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      const { stateCount, stateVariables } = this.state;
-      const newStateCount = (stateCount + 1) % 4;
-      this.setState({
-        ...this.state,
-        stateCount: newStateCount,
-        serverState: stateVariables[newStateCount],
-      });
-    }, 6000);
-  }
-
   render() {
-    const { serverState } = this.state;
+
+    const { storageValue, computationValue } = this.props.sillyRangeValues;
+    let serverState = "walking";
+
+    if (storageValue <= 25 && computationValue <= 25) {
+      serverState = "walking";
+    } else if (storageValue <= 50 && computationValue <= 50) {
+      serverState = "briskWalking";
+    } else if (storageValue <= 75 && computationValue <= 75) {
+      serverState = "running";
+    } else if (storageValue <= 100 && computationValue <= 100) {
+      serverState = "extreme";
+    } else {
+    }
 
     return (
       <div className='canvas'>
@@ -53,4 +48,8 @@ class CanvasContainer extends React.Component {
 
 }
 
-export default CanvasContainer;
+const mapStateToProps = createStructuredSelector({
+  sillyRangeValues: selectCurrentSillyRangeValues,
+});
+
+export default connect(mapStateToProps)(CanvasContainer);
