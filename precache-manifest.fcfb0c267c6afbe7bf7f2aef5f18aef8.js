2283d5190e29f0c2e4901c492f4a7c4d809d6d8c
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-09-20 03:18:41 +0530

    Updates

---
+++ precache-manifest.fcfb0c267c6afbe7bf7f2aef5f18aef8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270cbfcf899a1b71f3daef500df7cfc6",
+    "url": "/silly-server/index.html"
+  },
+  {
+    "revision": "eaba4ee1c190543d3f38",
+    "url": "/silly-server/static/css/main.fe7ce5b3.chunk.css"
+  },
+  {
+    "revision": "73de9acac2a5b88b6eb2",
+    "url": "/silly-server/static/js/2.b4bb0409.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/silly-server/static/js/2.b4bb0409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaba4ee1c190543d3f38",
+    "url": "/silly-server/static/js/main.af0ac120.chunk.js"
+  },
+  {
+    "revision": "d12899b51e8be39b3d2a",
+    "url": "/silly-server/static/js/runtime-main.d86260ea.js"
+  },
+  {
+    "revision": "7929e4f30330c740e450df0d104f02db",
+    "url": "/silly-server/static/media/backpack.7929e4f3.svg"
+  },
+  {
+    "revision": "3f5f4b278d0487eb60ff3c58798feefb",
+    "url": "/silly-server/static/media/file.3f5f4b27.svg"
+  },
+  {
+    "revision": "83a116ff527194d17d5cd3d503a7432f",
+    "url": "/silly-server/static/media/leftHand.83a116ff.svg"
+  },
+  {
+    "revision": "1960ae0a6c1ccea9a483b71e143e4f0a",
+    "url": "/silly-server/static/media/rightHand.1960ae0a.svg"
+  },
+  {
+    "revision": "3dfa25ee59089e5f28d9182d781bcefc",
+    "url": "/silly-server/static/media/server-2.3dfa25ee.svg"
+  },
+  {
+    "revision": "086ffc32c28dc1eb581c1d06dd140a12",
+    "url": "/silly-server/static/media/server-3.086ffc32.svg"
+  },
+  {
+    "revision": "d7154d51e869cf27faa187e3fdbd6b94",
+    "url": "/silly-server/static/media/server-4.d7154d51.svg"
+  },
+  {
+    "revision": "faadf3b5b0afb9104199786a2c7107d4",
+    "url": "/silly-server/static/media/server.faadf3b5.svg"
+  }
+]);
