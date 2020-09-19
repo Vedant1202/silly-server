2d3a9c487090224bfe262d6f310edc53fb01e669
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-09-20 03:08:03 +0530

    Completed with walking component

--- src/components/walking/walking.component.jsx
+++ src/components/walking/walking.component.jsx
@@ -5,12 +5,14 @@ import './walking.styles.scss';
 import leftHandWalking from '../../assets/samples/leftHand.svg';
 import serverBodyWalking from '../../assets/samples/server.svg';
 import rightHandWalking from '../../assets/samples/rightHand.svg';
+import BackPackComponent from '../backpack/backpack.component';
 
 const Walking = () => (
   <div className='walking'>
     <img src={serverBodyWalking} className="serverBodyWalking" alt="serverBody" />
     <img src={leftHandWalking} className="leftHandWalking" alt="leftHand" />
     <img src={rightHandWalking} className="rightHandWalking" alt="rightHand" />
+    <BackPackComponent/>
   </div>
 );
 
