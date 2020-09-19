aa715364f256406c28a14786c4ef95bf69084590
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-09-20 03:07:47 +0530

    Completed with running component

--- src/components/running/running.component.jsx
+++ src/components/running/running.component.jsx
@@ -5,12 +5,14 @@ import './running.styles.scss';
 import leftHand from '../../assets/samples/leftHand.svg';
 import serverBodyRunning from '../../assets/samples/server-3.svg';
 import rightHand from '../../assets/samples/rightHand.svg';
+import BackPackComponent from '../backpack/backpack.component';
 
 const Running = () => (
   <div className='brisk-walking'>
     <img src={serverBodyRunning} className="serverBodyRunning" alt="serverBody" />
     <img src={leftHand} className="leftHandRunning" alt="leftHand" />
     <img src={rightHand} className="rightHandRunning" alt="rightHand" />
+    <BackPackComponent/>
   </div>
 );
 
