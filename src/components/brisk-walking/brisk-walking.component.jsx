ad1bc88c9488e37ddd2e665950ace5d966da7df9
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-09-20 03:06:33 +0530

    Completed with brisk walking component

--- src/components/brisk-walking/brisk-walking.component.jsx
+++ src/components/brisk-walking/brisk-walking.component.jsx
@@ -5,12 +5,14 @@ import './brisk-walking.styles.scss';
 import leftHand from '../../assets/samples/leftHand.svg';
 import serverBodyBriskWalking from '../../assets/samples/server-2.svg';
 import rightHand from '../../assets/samples/rightHand.svg';
+import BackPackComponent from '../backpack/backpack.component';
 
 const BriskWalking = () => (
   <div className='brisk-walking'>
     <img src={serverBodyBriskWalking} className="serverBodyBriskWalking" alt="serverBody" />
     <img src={leftHand} className="leftHandBriskWalking" alt="leftHand" />
     <img src={rightHand} className="rightHandBriskWalking" alt="rightHand" />
+    <BackPackComponent/>
   </div>
 );
 
